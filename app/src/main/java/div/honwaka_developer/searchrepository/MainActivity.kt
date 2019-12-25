78f679745736acd452ee23d9a924038f3a25a6ae
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2019-12-26 06:19:47 +0900

    SearchViewの導入

--- app/src/main/java/div/honwaka_developer/searchrepository/MainActivity.kt
+++ app/src/main/java/div/honwaka_developer/searchrepository/MainActivity.kt
@@ -1,12 +1,31 @@
 package div.honwaka_developer.searchrepository
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchView = menu!!.findItem(R.id.search).actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+        return true
     }
 }
